4c11ffe1f0d9eaa808af2b44a5d8ec0bbde3768d
OlegAdmin <olegfil99@mail.ru>
2019-10-12 23:15:09 +0300

    Project created

---
+++ calculatorsubmodule2/calculatorsubmodule2/src/main/java/com/oleg/App2.java
@@ -0,0 +1,13 @@
+package com.oleg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World 2!" );
+    }
+}
